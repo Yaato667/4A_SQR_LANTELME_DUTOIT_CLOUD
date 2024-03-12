b01032f0cc2a7816f266bcba7b48251496b63a28
Txm <108979172+Yaato667@users.noreply.github.com>
2024-03-12 09:13:30 +0100

    Route ajouter tweet main.py

--- PROJET/main.py
+++ PROJET/main.py
@@ -1,55 +1,41 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import redis
+from urllib.parse import quote_plus
 
 app = Flask(__name__)
+CORS(app)  # Active CORS pour toute l'application
 redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 # Fonction pour enregistrer un tweet dans Redis
 def enregistrer_tweet(tweet_id, tweet_text, username, hashtags):
+    # Convertir la liste de hashtags en une chaîne de caractères
+    hashtags_str = ' '.join(hashtags)
+    
     tweet_data = {
         'tweet_id': tweet_id,
         'tweet_text': tweet_text,
         'username': username,
-        'hashtags': hashtags
+        'hashtags': hashtags_str,  # Utiliser la chaîne de caractères des hashtags
+        'retweet_count': 0  # Initialiser le compteur de retweets à 0
     }
     redis_client.hmset(f'tweet:{tweet_id}', tweet_data)
+    
+    # Enregistrer les hashtags associés au tweet
+    for hashtag in hashtags:
+        redis_client.sadd(f'h-hashtag:{hashtag}', f'tweet:{tweet_id}')
 
-# Fonction pour afficher tous les tweets
-@app.route('/tweets', methods=['GET'])
-def afficher_tweets():
-    keys = redis_client.keys('tweet:*')
-    tweets = [redis_client.hgetall(key) for key in keys]
-    return jsonify(tweets)
 
-# Fonction pour afficher les tweets liés à une personne
-@app.route('/tweets/<username>', methods=['GET'])
-def afficher_tweets_utilisateur(username):
-    keys = redis_client.keys('tweet:*')
-    user_tweets = [redis_client.hgetall(key) for key in keys if redis_client.hget(key, 'username') == username]
-    return jsonify(user_tweets)
-
-# Fonction pour retweeter un tweet
-@app.route('/tweets/retweet', methods=['POST'])
-def retweeter_tweet():
+# Route pour ajouter un tweet avec la méthode POST
+@app.route('/tweets', methods=['POST'])
+def ajouter_tweet():
     data = request.json
     tweet_id = data['tweet_id']
-    # Simuler le retweet en incrémentant le compteur de retweets
-    redis_client.hincrby(f'tweet:{tweet_id}', 'retweet_count', 1)
-    return jsonify({'message': f'Tweet {tweet_id} retweeté avec succès'})
-
-# Fonction pour afficher les sujets
-@app.route('/sujets', methods=['GET'])
-def afficher_sujets():
-    keys = redis_client.keys('h-hashtag:*')
-    sujets = [key.decode('utf-8').split(':')[-1] for key in keys]
-    return jsonify(sujets)
-
-# Fonction pour afficher les tweets liés à un sujet
-@app.route('/sujets/<hashtag>', methods=['GET'])
-def afficher_tweets_sujet(hashtag):
-    keys = redis_client.keys(f'h-hashtag:{hashtag}:*')
-    sujet_tweets = [redis_client.hgetall(key) for key in keys]
-    return jsonify(sujet_tweets)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    tweet_text = data['tweet_text']
+    username = data['username']
+    hashtags = data['hashtags']
+    
+    # Appel à la fonction pour enregistrer le tweet
+    enregistrer_tweet(tweet_id, tweet_text, username, hashtags)
+    
+    return jsonify({'message': f'Tweet {tweet_id} ajouté avec succès'})
